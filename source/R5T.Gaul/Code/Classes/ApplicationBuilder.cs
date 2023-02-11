9e8fca3047c8fca31622ef9be6065fabee2fcd93
davidcoats <david@getsafetycone.com>
2023-02-10 17:42:33 -0800

    Interim changes.

--- source/R5T.Gaul/Code/Classes/ApplicationBuilder.cs
+++ source/R5T.Gaul/Code/Classes/ApplicationBuilder.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-using R5T.Sardinia;
+using R5T.Chamavia;
 
 using R5T.Gaul.Extensions;
 
